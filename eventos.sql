e4a6f677b50b914d60e6a0f6dbb9af1f935332c1
Laura Andrea Rodríguez Mejía <91057900+laura2ndrea@users.noreply.github.com>
2024-10-28 08:58:19 -0500

    Update eventos.sql

--- eventos.sql
+++ eventos.sql
@@ -1,4 +1,4 @@
-USE pancito; 
+USE Pancito; 
 SET GLOBAL event_scheduler = ON;
 SHOW EVENTS;
 
@@ -35,11 +35,9 @@ CREATE EVENT actualizarInventario
 ON SCHEDULE EVERY 1 DAY STARTS '2024-10-28 19:30:00'
 DO
 BEGIN 
-	UPDATE Ingredientes_Presentaciones ip
-JOIN (
-    SELECT 
-        pi.id_ingrediente, 
-        SUM(v.cantidad) AS cantidad_vendida
+	UPDATE Ingredientes_Presentaciones ip 
+    JOIN (
+    SELECT pi.id_ingrediente, SUM(v.cantidad) AS cantidad_vendida
     FROM Ventas v
     JOIN Productos_Ingredientes pi ON v.id_producto = pi.id_producto
     JOIN Facturas f ON f.id = v.id_factura
@@ -54,5 +52,26 @@ SELECT * FROM InformesVentas;
 SELECT i.nombre, ip.cantidad
 FROM Ingredientes_Presentaciones ip
 JOIN Ingredientes i ON ip.id_ingrediente = i.id; 
+
+-- 3. Revisar si algún ingrediente está por debajo del nivel mínimo y generar una alerta.
+CREATE TABLE IF NOT EXISTS Alertas (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    fecha DATETIME NOT NULL, 
+    mensaje VARCHAR(200)
+);
+
+DELIMITER $ 
+CREATE EVENT alertaMinimo 
+ON SCHEDULE EVERY DAY STARTS '2024-10-27 19:30:00'
+BEGIN 
+	INSERT INTO Alertas (fecha, mensaje)
+    VALUES (NOW(), CONCAT('Hay bajo inventario de ', i.nombre, 'esta debajo del nivel mínimo. Cantidad actual: ', ip.cantidad))
+    FROM Ingredientes i 
+    JOIN Ingredientes_Presentaciones ip ON i.id = ip.id_ingrediente
+    WHERE ip.cantidad < 5; 
+END $
+DELIMITER ; 
+
+SELECT * FROM Alertas; 
 
 
